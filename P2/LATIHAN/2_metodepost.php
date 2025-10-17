c77f04e17c8664efd1cb78cb9b1ded63e4095b81
anastasya923 <tasyasaya13@gmail.com>
2025-10-17 13:01:49 +0700

    Add files via upload

---
+++ P2/LATIHAN/2_metodepost.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> FOrm Method Post</title>
+    </head>
+    <body>
+        <h1>Input</h1>
+        <form action="2_methodpostproses.php" method="post">
+            Masukkan nama : <input type="text" name="nama" size="25" maxlength="100">
+            <input type="submit" value="Proses">
+        </form>
+    </body>
+</html>
