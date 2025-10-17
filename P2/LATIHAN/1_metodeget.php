f21315cfe57037767e867cbd59fb0da9b8f8db2a
anastasya923 <tasyasaya13@gmail.com>
2025-10-17 13:00:33 +0700

    Create 1_metodeget.php

---
+++ P2/LATIHAN/1_metodeget.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Form method GET</title>
+    </head>
+    <body>
+        <h1>Input</h1>
+        <form action="1_methodgetProses.php" method="get">
+            Masukkan nama : <input type="text" name="nama" size="25" maxlength="100">
+            <input type="Submit" value="proses">
+        </form>
+    </body>
+</html>
